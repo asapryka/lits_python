77f3eaf019c8f2c3f8597ea22eb388aa43a062c5
asapryka <andriy.sapryka@hotschedules.com>
2018-04-23 14:20:46 +0300

    modified solution ToDo: check if this is formula or just parenthesis

--- command_line_arguments.py
+++ command_line_arguments.py
@@ -1,24 +1,28 @@
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(description='Process some formula and see if parenthesis are balanced.')
 parser.add_argument('string', metavar='N', type=str, nargs='+',
                     help='enter a formula (string) to process')
 
-args = parser.parse_args('(a-2)*(sqrt(4x)-6)**2')
-list = list(args.__str__())
+args = parser.parse_args(sys.argv[1:])
+list = sys.argv[1:]
+str = list[0]
+str = str.strip("'")
+
 new_list = []
 
-for n in list:
+for n in str:
     if n in("(", ")"):
         new_list.append(n)
 
-str = ''.join(new_list)
-print(str)
-l_side = str.count('(')
-r_side = str.count(')')
+print(new_list)
+str_final = ''.join(new_list)
+
+l_side = str_final.count('(')
+r_side = str_final.count(')')
 
 if l_side == r_side:
     print("True")
 else:
     print('False')
-
